5bd89c54c2a2638cde919fcfa0ef641012fbba17
yaroslav <yaroslav@DESKTOP-VF8ON2H>
2023-05-17 01:36:59 +0800

    Add project files.

---
+++ AreaOfFigure/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace AreaOfFigure
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            trackBar1 = new TrackBar();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            areaOfFigure = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(areaOfFigure);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(trackBar1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(615, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 450);
+            panel1.TabIndex = 1;
+            // 
+            // trackBar1
+            // 
+            trackBar1.BackColor = Color.White;
+            trackBar1.Location = new Point(3, 27);
+            trackBar1.Maximum = 500;
+            trackBar1.Minimum = 1;
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(194, 45);
+            trackBar1.TabIndex = 0;
+            trackBar1.TickFrequency = 10;
+            trackBar1.Value = 100;
+            trackBar1.ValueChanged += trackBar1_ValueChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(192, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Расстояние от центра до вершин:";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(609, 450);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(186, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Площадь закрашенной фигуры:";
+            // 
+            // areaOfFigure
+            // 
+            areaOfFigure.AutoSize = true;
+            areaOfFigure.Location = new Point(3, 90);
+            areaOfFigure.Name = "areaOfFigure";
+            areaOfFigure.Size = new Size(13, 15);
+            areaOfFigure.TabIndex = 2;
+            areaOfFigure.Text = "0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(815, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel1);
+            MinimumSize = new Size(250, 250);
+            Name = "Form1";
+            Text = "Form1";
+            SizeChanged += Form1_SizeChanged;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Panel panel1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private TrackBar trackBar1;
+        private Label areaOfFigure;
+        private Label label2;
+    }
+}
---
+++ AreaOfFigure/Form1.cs
@@ -0,0 +1,97 @@
+using System.Drawing.Drawing2D;
+using System.Globalization;
+
+namespace AreaOfFigure
+{
+    public partial class Form1 : Form
+    {
+        private int _distanceToApex = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DrawStar();
+        }
+
+        private void DrawStar()
+        {
+            var bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            var g = Graphics.FromImage(bmp);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            var centerX = pictureBox1.Width / 2;
+            var centerY = pictureBox1.Height / 2;
+
+            var diameter = 0.62f * _distanceToApex;
+            var pentagonRadius = 0.38f * _distanceToApex;
+
+            const float angle = 1.25664f;
+
+            var points = new Point[5];
+            points[0] = new Point(centerX, centerY - _distanceToApex);
+
+            for (var i = 1; i < 5; i++)
+            {
+                var x = Convert.ToInt32(centerX + (points[i - 1].X - centerX) * Math.Cos(angle) -
+                                        (points[i - 1].Y - centerY) * Math.Sin(angle));
+                var y = Convert.ToInt32(centerY + (points[i - 1].X - centerX) * Math.Sin(angle) +
+                                        (points[i - 1].Y - centerY) * Math.Cos(angle));
+                points[i] = new Point(x, y);
+            }
+
+            var pentagonPoints = new Point[5];
+            pentagonPoints[0] = new Point(centerX, Convert.ToInt32(centerY + pentagonRadius));
+
+            for (var i = 1; i < 5; i++)
+            {
+                var x = Convert.ToInt32(centerX + (pentagonPoints[i - 1].X - centerX) * Math.Cos(angle) -
+                                        (pentagonPoints[i - 1].Y - centerY) * Math.Sin(angle));
+                var y = Convert.ToInt32(centerY + (pentagonPoints[i - 1].X - centerX) * Math.Sin(angle) +
+                                        (pentagonPoints[i - 1].Y - centerY) * Math.Cos(angle));
+                pentagonPoints[i] = new Point(x, y);
+            }
+
+            var pentagonArea = calculateAreaOfPentagon(pentagonRadius, angle);
+            var circleArea = calculateAreaOfCircle(diameter);
+
+            areaOfFigure.Text = (pentagonArea - circleArea).ToString(CultureInfo.InvariantCulture);
+
+            g.FillPolygon(new SolidBrush(Color.FromArgb(100, Color.Red)), pentagonPoints);
+
+            g.FillEllipse(new SolidBrush(Color.FromArgb(255, Color.White)),
+                new Rectangle((int)(centerX - diameter / 2), (int)(centerY - diameter / 2), (int)diameter,
+                    (int)diameter));
+
+            var blackPen = new Pen(Color.FromArgb(255, 0, 0, 0), 3);
+
+            g.DrawLine(blackPen, points[0], points[2]);
+            g.DrawLine(blackPen, points[2], points[4]);
+            g.DrawLine(blackPen, points[4], points[1]);
+            g.DrawLine(blackPen, points[1], points[3]);
+            g.DrawLine(blackPen, points[3], points[0]);
+
+            pictureBox1.Image = bmp;
+        }
+
+        private double calculateAreaOfPentagon(float radius, float angle)
+        {
+            return 5f / 2f * radius * radius * Math.Sin(angle);
+        }
+
+        private double calculateAreaOfCircle(float diameter)
+        {
+            return Math.PI * ((diameter / 2) * (diameter / 2));
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            _distanceToApex = trackBar1.Value;
+            DrawStar();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            DrawStar();
+        }
+    }
+}
